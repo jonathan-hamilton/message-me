a9bb8604068e89763d99c39eabd1db003c82a288
jonathan-hamilton <jonathan_hamilton@me.com>
2020-12-23 11:52:36 -0800

    build user resource and seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Jon", password: "123")
+User.create(username: "Vanessa", password: "123")
+User.create(username: "Jack", password: "123")
+User.create(username: "Alexa", password: "123")
+User.create(username: "Kayla", password: "123")
